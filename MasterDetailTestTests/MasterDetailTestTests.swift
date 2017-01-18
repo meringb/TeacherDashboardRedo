6e707344b6aa3477a27668f841e109bd57d45e69
Brynna Mering <brynnamering@eduroam-171-10.dyn.carleton.edu>
2017-01-18 03:56:14 -0600

    Added project

---
+++ MasterDetailTestTests/MasterDetailTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  MasterDetailTestTests.swift
+//  MasterDetailTestTests
+//
+//  Created by Brynna Mering on 1/18/17.
+//  Copyright © 2017 Brynna Mering. All rights reserved.
+//
+
+import XCTest
+@testable import MasterDetailTest
+
+class MasterDetailTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
